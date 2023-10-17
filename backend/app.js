89c2ca65a7c6a9bab06809a998be9661a35d7060
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-10-17 13:57:21 -0400

    removed consolelog in the code

--- backend/app.js
+++ backend/app.js
@@ -67,7 +67,7 @@ if (!isProduction) {
 
   app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
-    console.error(err);
+    //console.error(err);
     res.json({
       title: err.title || 'Server Error',
       message: err.message,
