212d156fca40aed7499daaaf3d19990e95a8e265
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-10-24 17:25:21 -0400

    added back seeder file. build command without seeding.

--- backend/db/seeders/3-bookings.js
+++ backend/db/seeders/3-bookings.js
@@ -1,60 +1,60 @@
-// "use strict";
+"use strict";
 
-// const { Booking } = require("../backend/db/models");
+const { Booking } = require("../backend/db/models");
 
-// let options = {};
-// if (process.env.NODE_ENV === "production") {
-//   options.schema = process.env.SCHEMA; // define your schema in options object
-// }
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await Booking.bulkCreate(
-//       [
-//         {
-//           spotId: 1,
-//           userId: 2,
-//           startDate: "2021-01-01",
-//           endDate: "2021-01-02",
-//         },
-//         {
-//           spotId: 2,
-//           userId: 1,
-//           startDate: "2023-10-20",
-//           endDate: "2023-11-10",
-//         },
-//         {
-//           spotId: 3,
-//           userId: 1,
-//           startDate: "2023-03-03",
-//           endDate: "2023-03-04",
-//         },
-//         {
-//           spotId: 4,
-//           userId: 3,
-//           startDate: "2023-04-04",
-//           endDate: "2023-04-05",
-//         },
-//         {
-//           spotId: 2,
-//           userId: 3,
-//           startDate: "2023-12-04",
-//           endDate: "2023-12-05",
-//         },
-//       ],
-//       { validate: true }
-//     );
-//   },
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Booking.bulkCreate(
+      [
+        {
+          spotId: 1,
+          userId: 2,
+          startDate: "2021-01-01",
+          endDate: "2021-01-02",
+        },
+        {
+          spotId: 2,
+          userId: 1,
+          startDate: "2023-10-20",
+          endDate: "2023-11-10",
+        },
+        {
+          spotId: 3,
+          userId: 1,
+          startDate: "2023-03-03",
+          endDate: "2023-03-04",
+        },
+        {
+          spotId: 4,
+          userId: 3,
+          startDate: "2023-04-04",
+          endDate: "2023-04-05",
+        },
+        {
+          spotId: 2,
+          userId: 3,
+          startDate: "2023-12-04",
+          endDate: "2023-12-05",
+        },
+      ],
+      { validate: true }
+    );
+  },
 
-//   async down(queryInterface, Sequelize) {
-//     options.tableName = "Bookings";
-//     const Op = Sequelize.Op;
-//     return queryInterface.bulkDelete(
-//       options,
-//       {
-//         spotId: { [Op.in]: [1, 2, 3, 4] },
-//       },
-//       {}
-//     );
-//   },
-// };
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Bookings";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: { [Op.in]: [1, 2, 3, 4] },
+      },
+      {}
+    );
+  },
+};
--- backend/db/seeders/4-reviews.js
+++ backend/db/seeders/4-reviews.js
@@ -1,78 +1,78 @@
-// "use strict";
+"use strict";
 
-// const { Review } = require("../backend/db/models");
+const { Review } = require("../backend/db/models");
 
-// let options = {};
-// if (process.env.NODE_ENV === "production") {
-//   options.schema = process.env.SCHEMA; // define your schema in options object
-// }
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await Review.bulkCreate(
-//       [
-//         {
-//           spotId: 1,
-//           userId: 2,
-//           review: `This place is beautiful.`,
-//           stars: 4,
-//         },
-//         {
-//           spotId: 1,
-//           userId: 3,
-//           review: `This place is awesome.`,
-//           stars: 5,
-//         },
-//         {
-//           spotId: 2,
-//           userId: 1,
-//           review: `This place is terrible.`,
-//           stars: 1,
-//         },
-//         {
-//           spotId: 2,
-//           userId: 3,
-//           review: `This place is ugly.`,
-//           stars: 2,
-//         },
-//         {
-//           spotId: 3,
-//           userId: 1,
-//           review: `This place is so so.`,
-//           stars: 3,
-//         },
-//         {
-//           spotId: 3,
-//           userId: 2,
-//           review: `This place is alright.`,
-//           stars: 3,
-//         },
-//         {
-//           spotId: 4,
-//           userId: 2,
-//           review: `This place is fine.`,
-//           stars: 5,
-//         },
-//         {
-//           spotId: 4,
-//           userId: 3,
-//           review: `This place is great.`,
-//           stars: 5,
-//         },
-//       ],
-//       { validate: true }
-//     );
-//   },
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Review.bulkCreate(
+      [
+        {
+          spotId: 1,
+          userId: 2,
+          review: `This place is beautiful.`,
+          stars: 4,
+        },
+        {
+          spotId: 1,
+          userId: 3,
+          review: `This place is awesome.`,
+          stars: 5,
+        },
+        {
+          spotId: 2,
+          userId: 1,
+          review: `This place is terrible.`,
+          stars: 1,
+        },
+        {
+          spotId: 2,
+          userId: 3,
+          review: `This place is ugly.`,
+          stars: 2,
+        },
+        {
+          spotId: 3,
+          userId: 1,
+          review: `This place is so so.`,
+          stars: 3,
+        },
+        {
+          spotId: 3,
+          userId: 2,
+          review: `This place is alright.`,
+          stars: 3,
+        },
+        {
+          spotId: 4,
+          userId: 2,
+          review: `This place is fine.`,
+          stars: 5,
+        },
+        {
+          spotId: 4,
+          userId: 3,
+          review: `This place is great.`,
+          stars: 5,
+        },
+      ],
+      { validate: true }
+    );
+  },
 
-//   async down(queryInterface, Sequelize) {
-//     options.tableName = "Reviews";
-//     const Op = Sequelize.Op;
-//     return queryInterface.bulkDelete(
-//       options,
-//       {
-//         spotId: { [Op.in]: [1, 2, 3, 4] },
-//       },
-//       {}
-//     );
-//   },
-// };
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Reviews";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: { [Op.in]: [1, 2, 3, 4] },
+      },
+      {}
+    );
+  },
+};
