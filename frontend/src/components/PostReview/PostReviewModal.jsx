b80af2490150d8c136446764ae9109a715dc9777
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-12-14 09:33:31 -0500

    revise rating to star input selection.

--- frontend/src/components/PostReview/PostReviewModal.jsx
+++ frontend/src/components/PostReview/PostReviewModal.jsx
@@ -3,15 +3,21 @@ import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import './PostReviewModal.css'
 import { addReviewThunk } from '../../store/review';
+import RatingStars from './RatingStars';
 function PostNewReviewModal ({spotId}) {
 
     const dispatch = useDispatch();
     //const sessionUser = useSelector((state) => state.session.user);
     const [review, setReview] = useState('');
-    const [rating, setRating] = useState('');
+    const [rating, setRating] = useState(0);
     const [errors, setErrors] = useState({});
     const { closeModal } = useModal();
     //console.log(spotId)
+
+    const onChange = (num) => {
+        setRating(num);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors({});
@@ -57,25 +63,13 @@ function PostNewReviewModal ({spotId}) {
 
                 <rating/> */}
                 {errors.stars && (<div className='newspot-form-required-input'>{errors.stars}</div>)}
-                <div className='postreview-rating'>
-                    <input
-                        className='postreview-rating-inputbox'
-                        type="number"
-                        id="rating"
-                        name="rating"
-                        min="1"
-                        max="5"
-                        value={rating}
-                        onChange={(e) => setRating(e.target.value)}
-                    />
-                    <label htmlFor="rating">Stars</label>
-                </div>
 
+                <RatingStars rating={rating} onChange={onChange}/>
 
                 <button className='postreview-submit-button'
                     type='button'
                     onClick={handleSubmit}
-                    disabled={review.length < 10 || rating === ''}
+                    disabled={review.length < 10 || rating < 1}
                 >
                     Submit Your Review
                 </button>
