fc8408b28aadad60d85cff01788205de278c019d
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-12-18 19:28:12 -0500

    added console.log for backend xsrf-token.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -21,6 +21,7 @@ router.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   // Serve the frontend's index.html file at the root route
+  console.log(`trying to get xsrf-token - production`)
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
@@ -33,6 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
+    console.log(`trying to get xsrf-token - production that doesnt start with API`)
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
@@ -42,6 +44,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
+  console.log(`trying to get xsrf-token - not in production`)
   router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
