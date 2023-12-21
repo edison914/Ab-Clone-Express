cf84a184a0693fb5d353c9eb7ae3b40cf648bf80
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-12-21 08:33:15 -0500

    added console log for csrf token.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log(`production mode API is called`)
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
+    console.log('Generated CSRF Token:', csrfToken);
     res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
@@ -23,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
+    console.log('Generated CSRF Token:', csrfToken);
     console.log(`production mode none-API is called`)
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
@@ -36,6 +38,7 @@ if (process.env.NODE_ENV !== 'production') {
   console.log(`Development mode is called`)
   router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
+    console.log('Generated CSRF Token:', csrfToken);
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
       'XSRF-Token': csrfToken
